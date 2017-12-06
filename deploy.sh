1612f47130e0d388af0c4316369c7eeaadaa42c0
GuiltyMorishita <morilliantblue@gmail.com>
2017-12-06 12:04:44 +0900

    Change network to testnet
     pool_configs/litecoin.json | 12 ++++++------
     1 file changed, 6 insertions(+), 6 deletions(-)

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker push 139690869284.dkr.ecr.ap-northeast-1.amazonaws.com/nomp:latest
 # deploy
 ecs-cli compose --file docker-compose-nomp.yaml --project-name nomp create
 TASK_DEFINITION_ARNS=`aws ecs --region ap-northeast-1 list-task-definitions --sort DESC --family-prefix "nomp" | jq ".[][0]" | sed 's/"//g'`
-export CLUSTER_NAME=nomp
+CLUSTER_NAME=nomp
 aws ecs --region ap-northeast-1 update-service --cluster ${CLUSTER_NAME} --service nomp --task-definition ${TASK_DEFINITION_ARNS} --deployment-configuration maximumPercent=200,minimumHealthyPercent=50
